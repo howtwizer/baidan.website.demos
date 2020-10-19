84c44b2b92f8039d26925fc7a80062b5b18d063c
Alex Baidan <howtwizer@gmail.com>
2020-10-19 18:13:35 +0200

    add: config.hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,8 @@ Rails.application.configure do
 
   config.session_store :cache_store
 
+  config.hosts << "*.ondigitalocean.app"
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
