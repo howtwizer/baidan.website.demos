df9629f0bbfd08cdf521e7472a06db37a0d8c222
Alex Baidan <howtwizer@gmail.com>
2020-11-04 11:15:43 +0100

    add: quicksort algorithm

--- config/initializers/algorithms.rb
+++ config/initializers/algorithms.rb
@@ -1,2 +1,3 @@
 require "#{Rails.root}/lib/algorithms/sorting/insertion_sort"
 require "#{Rails.root}/lib/algorithms/sorting/selection_sort"
+require "#{Rails.root}/lib/algorithms/sorting/quick_sort"
